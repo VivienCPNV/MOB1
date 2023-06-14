4c20f82a7c820cb8117b48acc46072cfb34b6707
PICCIN Vivien <Vivien.PICCIN@cpnv.ch>
2023-06-14 09:18:49 +0200

    Added basic loading animations on home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 import 'package:weather/weather.dart';
 import 'package:cloudy/providers/geolocator.dart';
 import 'package:cloudy/providers/weather.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +14,7 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   Weather? weatherData;
+  bool loading = true;
 
   @override
   void initState() {
@@ -37,6 +39,10 @@ class HomePageState extends State<HomePage> {
             children: <Widget>[
               GestureDetector(
                 onTap: () async {
+                  setState() {
+                    loading = true;
+                  }
+
                   final result =
                       await Navigator.pushNamed(context, '/search') as String;
                   getWeatherData(result);
@@ -48,7 +54,11 @@ class HomePageState extends State<HomePage> {
                 ),
               ),
               GestureDetector(
-                  onTap: () async {
+                  onTap: () {
+                    setState() {
+                      loading = true;
+                    }
+
                     getWeatherData();
                   },
                   child: const Icon(
@@ -57,77 +67,87 @@ class HomePageState extends State<HomePage> {
                     color: Colors.white,
                   )),
             ]),
-        Expanded(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Text((() {
-              String townName = "Town";
-              if (weatherData != null) {
-                townName = weatherData?.areaName ?? 'Town';
-              }
-              return townName;
-            })(),
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 50,
-                  color: Colors.white,
-                )),
-            Text(DateFormat('EEEE').format(DateTime.now()),
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 40,
-                  color: Colors.white,
-                )),
-            Image(
-              image: (() {
-                return AssetImage(
-                    'assets/images/${weatherData?.weatherIcon ?? "01d"}.png');
-              })(),
-              width: 200,
-              height: 200,
-            ),
-            Text((() {
-              return "${weatherData?.temperature?.celsius?.toStringAsFixed(1) ?? 20} C";
-            })(),
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 25, color: Colors.white)),
-            Text((() {
-              return weatherData?.weatherMain ?? "Clear";
-            })(),
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                )),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              const Image(
-                image: AssetImage('assets/images/thermometer_low.png'),
-                width: 50,
-                height: 50,
-              ),
+        if (!loading)
+          Expanded(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
               Text((() {
-                return "${weatherData?.tempMin?.celsius?.toStringAsFixed(1) ?? 10} C";
+                String townName = "Town";
+                if (weatherData != null) {
+                  townName = weatherData?.areaName ?? 'Town';
+                }
+                return townName;
               })(),
+                  textAlign: TextAlign.center,
                   style: const TextStyle(
-                    fontSize: 25,
+                    fontSize: 50,
                     color: Colors.white,
                   )),
-              const Image(
-                image: AssetImage('assets/images/thermometer_high.png'),
-                width: 50,
-                height: 50,
-              ),
-              Text((() {
-                return "${weatherData?.tempMax?.celsius?.toStringAsFixed(1) ?? 30} C";
-              })(),
+              Text(DateFormat('EEEE').format(DateTime.now()),
+                  textAlign: TextAlign.center,
                   style: const TextStyle(
-                    fontSize: 25,
+                    fontSize: 40,
                     color: Colors.white,
                   )),
-            ]),
-          ],
-        ))
+              Image(
+                image: (() {
+                  return AssetImage(
+                      'assets/images/${weatherData?.weatherIcon ?? "01d"}.png');
+                })(),
+                width: 200,
+                height: 200,
+              ),
+              Text((() {
+                return "${weatherData?.temperature?.celsius?.toStringAsFixed(1) ?? 20} C";
+              })(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 25, color: Colors.white)),
+              Text((() {
+                return weatherData?.weatherMain ?? "Clear";
+              })(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  )),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Image(
+                      image: AssetImage('assets/images/thermometer_low.png'),
+                      width: 50,
+                      height: 50,
+                    ),
+                    Text((() {
+                      return "${weatherData?.tempMin?.celsius?.toStringAsFixed(1) ?? 10} C";
+                    })(),
+                        style: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        )),
+                    const Image(
+                      image: AssetImage('assets/images/thermometer_high.png'),
+                      width: 50,
+                      height: 50,
+                    ),
+                    Text((() {
+                      return "${weatherData?.tempMax?.celsius?.toStringAsFixed(1) ?? 30} C";
+                    })(),
+                        style: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        )),
+                  ]),
+            ],
+          )),
+        if (loading)
+          Container(
+              color: Colors.grey.shade900,
+              child: const Center(
+                  child: SpinKitChasingDots(
+                color: Colors.white,
+              )))
       ],
     );
   }
@@ -135,18 +155,20 @@ class HomePageState extends State<HomePage> {
   getWeatherData([String city = ""]) {
     try {
       if (city != "") {
-        WeatherData.getWeatherCityName(city).then((weather) {
+        WeatherData().getWeatherCityName(city).then((weather) {
           setState(() {
             weatherData = weather;
+            loading = false;
           });
         });
       } else {
         // TODO: Add caching
         CloudyGeoLocator.getCurrentLocation().then((position) {
           if (position != null) {
-            WeatherData.getWeatherByPosition(position).then((weather) {
+            WeatherData().getWeatherByPosition(position).then((weather) {
               setState(() {
                 weatherData = weather;
+                loading = false;
               });
             });
           }
