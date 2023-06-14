ed0ab1fa193d8a6218f19c5bb3e574592d29dc4e
PICCIN Vivien <Vivien.PICCIN@cpnv.ch>
2023-06-14 11:34:30 +0200

    Added time based weather request caching

--- lib/providers/weather.dart
+++ lib/providers/weather.dart
@@ -14,10 +14,13 @@ class WeatherData {
   WeatherFactory weatherFactory = WeatherFactory(API_KEY);
   double previousLatitude = 0.00;
   double previousLongitude = 0.00;
+  int timeOfLastRequest = 0;
   Weather? previousWeatherData = null;
 
   Future<Weather?> getWeatherByPosition(Position position) async {
     if (previousWeatherData != null) {
+      // Check if user is still relatively close to the cached request. If he's
+      // far enough away, we update the cached request.
       if ((previousLatitude - position.latitude).abs() >= 0.05 &&
           (previousLongitude - position.longitude).abs() >= 0.05) {
         previousWeatherData = await weatherFactory.currentWeatherByLocation(
@@ -25,7 +28,18 @@ class WeatherData {
         previousLongitude = position.longitude;
         previousLatitude = position.latitude;
       }
-    } else {
+      // Check if last update was 30 minutes ago (1'800'000 milliseconds)
+      else if ((DateTime.now().millisecondsSinceEpoch - timeOfLastRequest)
+              .abs() >=
+          1800000) {
+        previousWeatherData = await weatherFactory.currentWeatherByLocation(
+            position.latitude, position.longitude) as Weather?;
+        previousLongitude = position.longitude;
+        previousLatitude = position.latitude;
+      }
+    }
+    // No cached request so do request
+    else {
       previousWeatherData = await weatherFactory.currentWeatherByLocation(
           position.latitude, position.longitude) as Weather?;
       previousLongitude = position.longitude;
